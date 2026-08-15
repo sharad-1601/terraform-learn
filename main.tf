8f30e9097b61e3b599dd3c5c01cfb25a8e38e481
Sharad Shakya <sharadshakya2005@gmail.com>
2026-08-15 13:55:35 +0530

    Initial Terraform AWS infrastructure

---
+++ main.tf
@@ -0,0 +1,90 @@
+#s3 bucket
+
+resource "aws_s3_bucket" terra-bucket {
+  bucket = "sharad-terraform-bucket"
+}
+
+#aws-instance
+
+
+#key-pair
+
+resource "aws_key_pair" "mykey" {
+  
+  key_name = "sharad-terraform-ec2"
+  public_key = file("sharad-terraform-ec2.pub")
+}
+
+
+#vpc
+
+resource "aws_default_vpc" "default" {
+  
+}
+
+
+#security group
+
+resource "aws_security_group" "terraform-sg" {
+  name = "sharad-terraform-sg"
+  description = "this will add a sg to tf generated file"
+  vpc_id = aws_default_vpc.default.id    #interpolation
+
+  #inbound rules
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "ssh opened"
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "http opened"
+  }
+
+  #outbound rules
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "all access open outbound"
+  }
+
+
+  tags = {
+    "Name" = "sharad-terraform-sg"
+  }
+}
+
+
+#instance
+
+
+resource "aws_instance" "sharad-terraform" {
+  
+  key_name = aws_key_pair.mykey.key_name
+  vpc_security_group_ids = [aws_security_group.terraform-sg.id]
+  instance_type = var.ec2_instance_type
+  # ami = "ami-04bc53b7a499f5d37"  #aws-linux
+  ami = var.ec2_ami_id
+
+
+
+  root_block_device {
+    volume_size = var.ec2_root_storage_size
+    volume_type = "gp3"
+  }
+
+  tags = {
+    "Name" = "sharad-terra-devops"
+  }
+
+}
---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "ec2_instance_type" {
+  default = "t3.micro"
+  type = string
+}
+
+
+variable "ec2_root_storage_size" { 
+  default = 8
+  type = number
+}
+
+
+variable "ec2_ami_id" {
+  default = "ami-04bc53b7a499f5d37"
+  type = string
+}
