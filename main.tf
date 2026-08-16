79b96558164acaaa2549f8f7d2b488bbfcde8013
Sharad Shakya <sharadshakya2005@gmail.com>
2026-08-16 13:08:59 +0530

    conditional arguments (? :)

--- main.tf
+++ main.tf
@@ -72,8 +72,9 @@ resource "aws_instance" "sharad-terraform" {
   # count = 4   #meta argument(means it create the instances equesl to count).
 
 for_each = tomap({
-  sharad-terra-devops-db = "t3.small"
-  sharad-terra-devops-app = "t3.micro"
+  sharad-terra-devops-db = "dev"
+  sharad-terra-devops-app = "prod"
+  sharad-terraform-devops-app2 = "prod"
 })    #another meta argument for_each used as map(key-pair) to give different name and sizes to the instnces
 
 depends_on = [ aws_default_vpc.default, aws_key_pair.mykey ] #another meta argument which defines the plan creation means which create first then anyother ything
@@ -81,7 +82,7 @@ depends_on = [ aws_default_vpc.default, aws_key_pair.mykey ] #another meta argum
 
   key_name = aws_key_pair.mykey.key_name
   vpc_security_group_ids = [aws_security_group.terraform-sg.id]
-  instance_type = each.value
+  instance_type = each.value == "prod" ? var.ec2_instance_type : "t3.small"
   # ami = "ami-04bc53b7a499f5d37"  #aws-linux
   ami = var.ec2_ami_id
   user_data = file("install_nginx.sh")
@@ -89,12 +90,13 @@ depends_on = [ aws_default_vpc.default, aws_key_pair.mykey ] #another meta argum
 
 
   root_block_device {
-    volume_size = var.ec2_root_storage_size
+    volume_size = each.value == "prod" ? var.ec2_root_storage_size : 10
     volume_type = "gp3"
   }
 
   tags = {
     "Name" = each.key
+    "Enviroment" = each.value
   }
 
 }
